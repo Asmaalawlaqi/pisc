d809df5e58c8755c88123cec796035c20d38f092
Asmaalawlaqi <143976152+Asmaalawlaqi@users.noreply.github.com>
2024-06-15 01:45:20 +0400

    Update wdmatch.c

--- wdmatch.c
+++ wdmatch.c
@@ -1,1 +1,27 @@
 
+#include <unistd.h>
+
+int main(int ac , char **av)
+{
+int i =0;
+int j = 0;
+if(ac == 3)
+{
+while(av[2][i])
+{
+  if(av[1][j] == av[2][i])
+    j++;
+  if(av[1][i])
+  {
+j = 0;
+    while(av[1][j])
+      {
+
+        write(1, &av[1][j] , 1);
+        j++;
+      } 
+    break;
+  }
+  i++;
+}
+}
